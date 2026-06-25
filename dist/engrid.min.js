def85b6cff2551cefb950c8049e8891d5a247e47
plpmd <pedroluanpd@gmail.com>
2026-06-25 08:40:54 -0300

    feat: only add new style to new-signup-lightbox

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, June 25, 2026 @ 07:37:17 ET
+ *  Date: Thursday, June 25, 2026 @ 07:40:34 ET
  *  By: pedroluan
  *  ENGrid styles: v0.25.8
  *  ENGrid scripts: v0.25.9
