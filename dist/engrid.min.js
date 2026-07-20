c01ffa39652688ac0a6ed991f351b87e48f9452b
plpmd <pedroluanpd@gmail.com>
2026-07-20 12:18:54 -0300

    feat: add new height

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, June 25, 2026 @ 13:38:16 ET
+ *  Date: Monday, July 20, 2026 @ 11:17:35 ET
  *  By: pedroluan
  *  ENGrid styles: v0.25.8
  *  ENGrid scripts: v0.25.9
