a7fa62f32576d6da61aedb613bf84fb0c3457b90
Fernando Santos <fern@ndo.io>
2026-08-06 11:48:39 -0400

    Browserlist Update

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, August 6, 2026 @ 11:16:38 ET
+ *  Date: Thursday, August 6, 2026 @ 11:48:23 ET
  *  By: fernando
  *  ENGrid styles: v0.27.0
  *  ENGrid scripts: v0.27.1
